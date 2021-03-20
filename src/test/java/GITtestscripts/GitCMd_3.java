6fd12c64641be66d7c5c8878c7bd1b41ebbf9a76
HALESHAKS <haleshks95@gmail.com>
2021-03-20 21:05:59 +0530

    second commit

---
+++ src/test/java/GITtestscripts/GitCMd_3.java
@@ -0,0 +1,11 @@
+package GITtestscripts;
+
+import javax.sound.midi.SysexMessage;
+
+public class GitCMd_3 {
+
+	public static void main(String[] args) {
+System.out.println("class3 created");
+	}
+
+}
---
+++ src/test/java/GITtestscripts/GitCmd_4.java
@@ -0,0 +1,9 @@
+package GITtestscripts;
+
+public class GitCmd_4 {
+
+	public static void main(String[] args) {
+System.out.println("class4 created");
+	}
+
+}
