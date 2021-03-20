ce10d452e3f7a60f12013171b5e67cb1d093ef06
HALESHAKS <haleshks95@gmail.com>
2021-03-20 20:56:51 +0530

    first commit

---
+++ src/test/java/GITtestscripts/GitCMd_1.java
@@ -0,0 +1,11 @@
+package GITtestscripts;
+
+
+public class GitCMd_1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("class1created");
+	}
+
+}
---
+++ src/test/java/GITtestscripts/GitCMd_2.java
@@ -0,0 +1,11 @@
+package GITtestscripts;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class GitCMd_2 {
+
+	public static void main(String[] args) {
+		System.out.println("class2 created");
+	}
+
+}
